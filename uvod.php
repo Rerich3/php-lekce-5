4717ac9b2e0935c7cab90a23bf86d524975eb212
rerich3 <miroslav.imris@gmail.com>
2019-10-31 18:56:42 +0100

    demo

--- uvod.php
+++ uvod.php
@@ -13,6 +13,20 @@ public function  __construct($products, $price, $customer)
 }
 }
 $products = ['pracka AEG', 'Zehlicka Zanussi'];
-$order = new Order($products, 12000, 'Josef Novak');
+$order1 = new Order($products, 12000, 'Josef Novak');
 
-print $order ->customer;
+print $order1 ->customer;
+print $order1->products[0,1]
+foreach ($order1->products as $item){
+    echo $item;
+    echo '<br>';
+}
+//chceme znizit objednavku o 100kc
+
+//$novaCena = $order->price;
+//$snizenaCena = $novaCena - 100;
+//$order->price= $snizenaCena;
+
+//zapisanie tohto na 1 riadok
+
+$order1 ->price = $order1->price - 100;
